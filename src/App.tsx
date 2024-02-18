f9b8e826b23837047bcd75b499a24ef7ec9d247e
Chase Harrison <ChaseH88@gmail.com>
2024-02-18 14:40:32 -0600

    adding react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CreateUserPage } from './pages/CreateUserPage';
 
 function App() {
   return (
     <div className="App">
-      <CreateUserPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/create-user" element={<CreateUserPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
