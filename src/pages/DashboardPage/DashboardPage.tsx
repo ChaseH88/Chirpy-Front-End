968800ebea14945e25d2dd14b2c5013fbd39423d
Chase Harrison <ChaseH88@gmail.com>
2024-02-19 16:10:56 -0600

    adding inital postbox logic and test creation

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,13 +1,56 @@
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { GET_POSTS } from './queries';
 import { useAppData } from '../../hooks/useAppData';
 import { useAuth } from '../../hooks/useAuth';
 import { Posts } from '../../components/Posts/Posts';
+import { CREATE_POST_MUTATION } from './mutations';
+import { Form, FormInput } from '../../components/Form';
+import { useForm } from 'react-hook-form';
 
 const DashboardPage = () => {
-  const { loading, error, data } = useQuery(GET_POSTS);
+  const { loading: getPostLoading, error, data } = useQuery(GET_POSTS);
+  const [createPost, { loading: createPostLoading }] =
+    useMutation(CREATE_POST_MUTATION);
   const { currentUser } = useAppData();
   const { logout } = useAuth();
+  const { getValues, setValue } = useForm({
+    defaultValues: {
+      content: '',
+    },
+    reValidateMode: 'onChange',
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const values = getValues();
+    if (!values.content) {
+      alert('Please fill out all fields');
+      return;
+    }
+    await createPost({
+      variables: {
+        data: {
+          content: values.content,
+          postedBy: currentUser!.id,
+        },
+      },
+      refetchQueries: [{ query: GET_POSTS }],
+    });
+    setValue('content', '');
+  };
+
+  const inputs: FormInput[] = [
+    {
+      name: 'content',
+      type: 'text',
+      placeholder: 'Enter your post',
+      required: true,
+      label: 'Post',
+      value: getValues('content'),
+      onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
+        setValue('content', e.target.value),
+    },
+  ];
 
   return (
     <div>
@@ -15,7 +58,8 @@ const DashboardPage = () => {
       <h3>{`Welcome ${currentUser!.username}`}</h3>
       <p>Welcome to the dashboard</p>
       <button onClick={logout}>Logout</button>
-      <Posts posts={data?.posts} headingText="All Posts" />
+      <Form inputs={inputs} onSubmit={handleSubmit} submitText="Login" />
+      <Posts posts={data?.allPosts} headingText="All Posts" />
     </div>
   );
 };
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,4 +1,5 @@
 export interface UserModelInterface {
+  id: string;
   email: string;
   password: string;
   username: string;
