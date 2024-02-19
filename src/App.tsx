d877691a566401bb37a71c71df7dfd4f0f52e288
Chase Harrison <ChaseH88@gmail.com>
2024-02-18 22:53:01 -0600

    uninstall react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
-import { ReactQuery } from './providers/ReactQuery';
 import { AppDataProvider } from './providers/AppData';
 import { Routes as AppRoutes } from './components/Routes';
 
 const App = (): JSX.Element => (
   <div className="App">
-    <ReactQuery>
-      <AppDataProvider>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </AppDataProvider>
-    </ReactQuery>
+    <AppDataProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </AppDataProvider>
   </div>
 );
 
--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,16 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
-
 const DashboardPage = () => {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['repoData'],
-    queryFn: () =>
-      fetch('https://api.github.com/repos/TanStack/query').then((res) =>
-        res.json()
-      ),
-  });
-
-  console.log({ isLoading, error, data });
-
   return (
     <div>
       <h1>Dashboard</h1>
