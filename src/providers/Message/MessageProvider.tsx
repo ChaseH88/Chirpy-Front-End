c339b141e64579b9dac6e0ee1df2c4bd00ab0b5e
Chase Harrison <ChaseH88@gmail.com>
2024-03-15 23:19:39 -0500

    improve message notification

--- src/providers/Message/MessageProvider.tsx
+++ src/providers/Message/MessageProvider.tsx
@@ -7,6 +7,9 @@ import { useAppData } from "../../hooks/useAppData";
 import { createContext } from "react";
 import { SEND_MESSAGE_MUTATION } from "./mutations";
 import { CURRENT_USER_QUERY } from "../AppData/queries";
+import { Box, Typography } from "@mui/material";
+import { useLocation, useNavigate } from "react-router-dom";
+import { QUERY_PARAM } from "../../components/Inbox/Inbox";
 
 export interface MessageContextInterface {
   messages?: MessageModelInterface[];
@@ -34,12 +37,38 @@ const MessageProvider = ({
   const { isLoggedIn } = useAuth();
   const { addToMessagesAction, currentUser } = useAppData();
   const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useSubscription(MESSAGE_SUBSCRIPTION, {
     skip: !isLoggedIn,
     onData: ({ data: { data } }) => {
+      const messagePagePath = `/messages?${QUERY_PARAM}=${data.messageSent.fromId?.id}`;
+      const params = new URLSearchParams(window.location.search);
+      const fromUser = params.get(QUERY_PARAM);
       addToMessagesAction(data.messageSent);
-      enqueueSnackbar("New message received", { variant: "info" });
+      if (
+        `${location.pathname}?${QUERY_PARAM}=${fromUser}` !== messagePagePath
+      ) {
+        enqueueSnackbar(
+          <Box
+            onClick={() => navigate(messagePagePath)}
+            sx={{
+              cursor: "pointer",
+            }}
+          >
+            <Typography variant="h6" m={0} fontWeight="bold" fontSize="0.9rem">
+              New message from {data.messageSent.fromId?.username}
+            </Typography>
+            <Typography variant="body1" fontSize="0.8rem" m={0}>
+              {data.messageSent.content.length > 20
+                ? `${data.messageSent.content.slice(0, 20)}...`
+                : `${data.messageSent.content}`}
+            </Typography>
+          </Box>,
+          { variant: "info" }
+        );
+      }
     },
   });
 
